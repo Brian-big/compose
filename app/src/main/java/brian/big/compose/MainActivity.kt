5a585c00f1adf3a59dc5e704af4a995d19151c51
Brian-big <briannbig@gmail.com>
2021-11-02 15:27:54 +0300

    expand/colapse list contents

--- app/src/main/java/brian/big/compose/MainActivity.kt
+++ app/src/main/java/brian/big/compose/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,7 +14,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -63,7 +64,8 @@ fun MessageCard(msg: Message){
                     .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
             )
             Spacer(modifier = Modifier.width(8.dp))
-            Column {
+        var isExpanded by remember{ mutableStateOf(false)}
+            Column(modifier = Modifier.clickable{isExpanded = !isExpanded}) {
                 Text(
                     text = msg.author, color = MaterialTheme.colors.secondaryVariant,
                     style = MaterialTheme.typography.subtitle2
@@ -73,6 +75,7 @@ fun MessageCard(msg: Message){
                     Text(
                         text = msg.body,
                         modifier = Modifier.padding(all = 4.dp),
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                         style = MaterialTheme.typography.body2
                     )
                 }

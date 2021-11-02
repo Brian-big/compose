39dc3c98eb3538c0e46b9aef72217e8466d08a57
Brian-big <briannbig@gmail.com>
2021-11-02 14:15:54 +0300

    Modifiers

--- app/src/main/java/brian/big/compose/MainActivity.kt
+++ app/src/main/java/brian/big/compose/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import brian.big.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,9 +33,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MessageCard(msg: Message){
     Row {
-        Image(painter = painterResource(id = R.drawable.admin_ic), contentDescription = "contact profile picture")
+        Image(painter = painterResource(id = R.drawable.admin_ic), contentDescription = "contact profile picture",
+        modifier = Modifier
+            .size(40.dp)
+            .clip(CircleShape))
+        Spacer(modifier = Modifier.width(8.dp))
         Column {
             Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
     }

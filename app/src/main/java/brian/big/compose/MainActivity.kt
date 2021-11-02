9761ff857ef7e95f9f7a20037f675dbf3f84029e
Brian-big <briannbig@gmail.com>
2021-11-02 11:36:54 +0300

    first composable function

--- app/src/main/java/brian/big/compose/MainActivity.kt
+++ app/src/main/java/brian/big/compose/MainActivity.kt
@@ -3,18 +3,19 @@ package brian.big.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import brian.big.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Start Composing")
+            MessageCard(name = "Android")
         }
     }
+}
+
+@Composable
+fun MessageCard(name: String){
+    Text(text = "Hello $name")
 }
